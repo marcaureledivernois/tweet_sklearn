43abaf0c2930c068a7982e6fd24e3b981f523642
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-20 01:11:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import cloudpickle as cp
 from preprocessing import preprocess
 import streamlit as st
 
-#orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
+@st.cache
 ros_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/ros_fit.sav"))
+#orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
 
 st.title('StockTwits Sentiment classifier')
 text = st.text_area(label="Enter message")
