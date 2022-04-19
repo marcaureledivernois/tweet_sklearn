6491dd8df320dc8d13c4ccfb5f63c09fdc3fdbdf
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-20 00:07:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,22 @@ import streamlit as st
 
 orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
 
-text = "Hello my nAme is marc-aurele, i'd love to. would you like to? y'all nice 3."
-clean = preprocess(text)
+st.title('StockTwits Sentiment classifier')
+text = st.text_area(label="Enter message")
 
-bullish_proba = orig_mod.predict_proba([text])[0][0]
+if text:
+    clean = preprocess(text)
 
-st.write(bullish_proba)
+    bullish_proba = orig_mod.predict_proba([text])[0][0]
+
+    if bullish_proba > 0.72:
+        sent = 'Bullish'
+    elif 0.5 < bullish_proba < 0.72:
+        sent = 'Neutral'
+    elif bullish_proba < 0.5:
+        sent = 'Bearish'
+    else:
+        sent = 'error'
+
+    st.write('The score of your message is :', bullish_proba)
+    st.write('Given our optimal thresholds, the message is classified as', sent)
