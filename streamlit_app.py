03a2b7e017c271555b894b04f5af761a097cd326
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-20 00:25:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import cloudpickle as cp
 from preprocessing import preprocess
 import streamlit as st
 
-orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
+#orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
+ros_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/ros_fit.sav"))
 
 st.title('StockTwits Sentiment classifier')
 text = st.text_area(label="Enter message")
@@ -11,13 +12,13 @@ text = st.text_area(label="Enter message")
 if text:
     clean = preprocess(text)
 
-    bullish_proba = orig_mod.predict_proba([text])[0][1]
+    bullish_proba = ros_mod.predict_proba([text])[0][1]
 
-    if bullish_proba > 0.72:
+    if bullish_proba > 0.6:
         sent = 'Bullish'
-    elif 0.5 < bullish_proba < 0.72:
+    elif 0.4 < bullish_proba < 0.6:
         sent = 'Neutral'
-    elif bullish_proba < 0.5:
+    elif bullish_proba < 0.4:
         sent = 'Bearish'
     else:
         sent = 'error'
