b4843bb50c37a7f447cff9d74b57690b7b17a712
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-19 23:21:36 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+import cloudpickle as cp
+from preprocessing import preprocess
+import streamlit
+
+orig_mod = cp.load(urlopen("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/original_fit.sav"))
+
+text = "Hello my nAme is marc-aurele, i'd love to. would you like to? y'all nice 3."
+clean = preprocess(text)
+
+bullish_proba = orig_mod.predict_proba([text])[0][0]
+
