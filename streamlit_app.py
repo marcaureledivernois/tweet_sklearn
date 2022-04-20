69655eca5ccf55b88658980cd8b3930309e30fff
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-20 10:56:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import cloudpickle as cp
 from preprocessing import preprocess
 import streamlit as st
 
-#@st.experimental_memo
+@st.experimental_memo
 def load_model(url_model):
     return cp.load(urlopen(url_model))
 
