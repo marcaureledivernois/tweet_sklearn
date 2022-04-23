387194750c2af88654c1fc89888f2e62448decdd
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-23 15:49:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,50 +14,53 @@ def load_model(url_model):
 
 mod = load_model("https://github.com/marcaureledivernois/tweet_sklearn/releases/download/v1.0/ros_fit.sav")
 
-st.title('StockTwits Sentiment classifier')
-st.subheader('*By Marc-Aurèle Divernois*')
-st.write('Sentiment classifier trained on 90 million messages from Stocktwits.com. Messages are classified into bullish, neutral or bearish classes.')
-text = st.text_area(label="Enter message")
+st.title('Tweets Sentiment Analysis - Interactive App')
+st.write('This NLP application is trained on 90 million tweets scraped from the API of Stocktwits.com from 2010 to 2020. ')
+option = st.selectbox('What tool do you want to use?',('Sentiment Classifier', 'Polarity Time-Series', 'Download Data'))
+#st.subheader('*By Marc-Aurèle Divernois*')
 
-if text:
-    clean = preprocess(text)
+if option == "Sentiment Classifier":
+    text = st.text_area(label="Enter message")
 
-    bullish_proba = mod.predict_proba([text])[0][1]
+    if text:
+        clean = preprocess(text)
 
-    if bullish_proba > 0.6:
-        sent = 'Bullish'
-    elif 0.4 < bullish_proba < 0.6:
-        sent = 'Neutral'
-    elif bullish_proba < 0.4:
-        sent = 'Bearish'
-    else:
-        sent = 'error'
-    
-    st.write('**Your message :**' , text)
-    st.write('**Preprocessed message :**' , clean)
-    st.write('**This message is classified as :**', sent)
-   
-    feature_names = np.array(mod.named_steps["tfidfvectorizer"].get_feature_names())
-    coefs = mod.named_steps["logisticregression"].coef_.flatten()
+        bullish_proba = mod.predict_proba([text])[0][1]
 
-    feat_importance = np.multiply(mod.named_steps.tfidfvectorizer.transform([text]).toarray(), mod.named_steps.logisticregression.coef_)
-    feat_importance = feat_importance[feat_importance != 0 ]
+        if bullish_proba > 0.6:
+            sent = 'Bullish'
+        elif 0.4 < bullish_proba < 0.6:
+            sent = 'Neutral'
+        elif bullish_proba < 0.4:
+            sent = 'Bearish'
+        else:
+            sent = 'error'
 
-    # Zip coefficients and names together and make a DataFrame
-    zipped = zip(feature_names[np.nonzero(mod.named_steps.tfidfvectorizer.transform([text]).toarray()[0])[0]], feat_importance)
-    dfz = pd.DataFrame(zipped, columns=["feature", "value"])# Sort the features by the absolute value of their coefficient
-    dfz["abs_value"] = dfz["value"].apply(lambda x: abs(x))
-    dfz["colors"] = dfz["value"].apply(lambda x: "green" if x > 0 else "red")
-    dfz = dfz.sort_values("abs_value", ascending=False)
+        st.write('**Your message :**' , text)
+        st.write('**Preprocessed message :**' , clean)
+        st.write('**This message is classified as :**', sent)
 
-    fig, ax = plt.subplots(1, 1, figsize=(12, 7))
-    sns.barplot(x="feature",
-                y="value",
-                data=dfz.head(20),
-               palette=dfz.head(20)["colors"])
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=90, fontsize=20)
-    ax.set_title("Feature importance", fontsize=25)
-    ax.set_ylabel("Coef", fontsize=22)
-    ax.set_xlabel("Feature Name", fontsize=22)
-    st.pyplot(fig)
+        feature_names = np.array(mod.named_steps["tfidfvectorizer"].get_feature_names())
+        coefs = mod.named_steps["logisticregression"].coef_.flatten()
+
+        feat_importance = np.multiply(mod.named_steps.tfidfvectorizer.transform([text]).toarray(), mod.named_steps.logisticregression.coef_)
+        feat_importance = feat_importance[feat_importance != 0 ]
+
+        # Zip coefficients and names together and make a DataFrame
+        zipped = zip(feature_names[np.nonzero(mod.named_steps.tfidfvectorizer.transform([text]).toarray()[0])[0]], feat_importance)
+        dfz = pd.DataFrame(zipped, columns=["feature", "value"])# Sort the features by the absolute value of their coefficient
+        dfz["abs_value"] = dfz["value"].apply(lambda x: abs(x))
+        dfz["colors"] = dfz["value"].apply(lambda x: "green" if x > 0 else "red")
+        dfz = dfz.sort_values("abs_value", ascending=False)
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        sns.barplot(x="feature",
+                    y="value",
+                    data=dfz.head(20),
+                   palette=dfz.head(20)["colors"])
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90, fontsize=20)
+        ax.set_title("Feature importance", fontsize=25)
+        ax.set_ylabel("Coef", fontsize=22)
+        ax.set_xlabel("Feature Name", fontsize=22)
+        st.pyplot(fig)
 
