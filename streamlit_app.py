47794840aa50c297c45adf64a217201cc9c85592
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2022-04-23 17:42:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if option == "Polarity Time-Series":
         return pd.read_csv(filename)
     
     sentratio_and_price = load_df('data/sentratio_and_price.csv')
-    eventlist = load_df('data/eventlist.csv)
+    eventlist = load_df('data/eventlist.csv')
     def plot_activity(ticker):
         cut_act = sentratio_and_price[(sentratio_and_price['ticker']==ticker) & (sentratio_and_price['businessday']==True)]
         cut_event = eventlist[eventlist['ticker']==ticker]
